28223af714dc90bc22f882c3182c24dca46ff5f7
Juan <juan.giraldo@celuweb.com>
2026-04-07 15:14:36 -0500

    feat: enhance SintiaCard with customizable properties for background color, border radius, border, and box shadow

--- lib/atoms/sintia_card.dart
+++ lib/atoms/sintia_card.dart
@@ -5,21 +5,30 @@ class SintiaCard extends StatelessWidget {
   const SintiaCard({
     super.key,
     required this.child,
+    this.backgroundColor,
+    this.borderRadius,
+    this.border,
+    this.boxShadow,
     this.padding = const EdgeInsets.all(SintiaSizes.size1),
   });
 
   final Widget child;
   final EdgeInsetsGeometry padding;
+  final Color? backgroundColor;
+  final BorderRadius? borderRadius;
+  final Border? border;
+  final List<BoxShadow>? boxShadow;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: padding,
       decoration: BoxDecoration(
-        color: context.colorScheme.surface,
-        borderRadius: BorderRadius.circular(SintiaSizes.radius1),
-        border: Border.all(color: AppColors.scale02),
-        boxShadow: SintiaShadows.s1,
+        color: backgroundColor ?? context.colorScheme.surface,
+        borderRadius:
+            borderRadius ?? BorderRadius.circular(SintiaSizes.radius1),
+        border: border ?? Border.all(color: AppColors.scale02),
+        boxShadow: boxShadow ?? SintiaShadows.s1,
       ),
       child: child,
     );
